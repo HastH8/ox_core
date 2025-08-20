c5ed71d13d0536fe1a313b74d77246a13f5a1eae
Hast <hast.herish@yahoo.com>
2025-08-20 13:24:38 -0700

    Update init.lua

--- lib/server/init.lua
+++ lib/server/init.lua
@@ -1,3 +1,9 @@
 require 'lib.server.player'
 require 'lib.server.vehicle'
 require 'lib.server.account'
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if resourceName == GetCurrentResourceName() then
+        print("^2Credits: ^7Overextended Team (Ox Core & Libraries) | Andyyy (ND Resources) | Ked.ss (Compatibility & Custom Features)")
+    end
+end)
